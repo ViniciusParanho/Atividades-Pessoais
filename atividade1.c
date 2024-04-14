e88a118011ef4cb5e0843f9eb80e18aeec627341
Vinicius Paranho Ribeiro <viniciusparanho@gmail.com>
2024-04-14 16:28:36 -0300

    Create atividade1.c

---
+++ atividade1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+  
+  int minhaaltura;
+  
+  int minhaidade;
+  
+  int meupeso ;
+
+  minhaaltura = 5*3;
+  minhaidade = 19;
+  meupeso = 73;
+  printf ("Minha altura é: %i \n", minhaaltura);
+  printf ("Minha Idade é : %i\n", minhaidade);
+  printf ("Meu Peso é : %i\n", meupeso);
+  
+  
+  return 0 ;
+  
+  
+}
