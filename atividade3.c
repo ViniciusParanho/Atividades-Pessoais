5ff833223e3705f2ffc2fac1854cdf05dcaa1f64
Vinicius Paranho Ribeiro <viniciusparanho@gmail.com>
2024-04-14 16:29:35 -0300

    Create atividade3.c

---
+++ atividade3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int minhaaltura = 5;
+
+  int minhaidade = 3;
+
+  int meupeso = 2; 
+
+  int soma = minhaaltura + minhaidade + meupeso;
+
+  printf("A soma dos numeros é:%i\n",soma);
+  
+
+
+
+
+
+  return 0;
+  
+}
