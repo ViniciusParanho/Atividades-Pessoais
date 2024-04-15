a871acbb98fbb00562294c6c523ad2ac7f548dd1
Vinicius Paranho Ribeiro <viniciusparanho@gmail.com>
2024-04-15 01:32:20 -0300

    Create atividade5.c

---
+++ atividade5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+  
+float num1=5;
+float num2= 2;
+  float resultado = num1/num2;
+  printf("o resultado é:%f\n", resultado);
+  
+  
+
+
+
+
+
+
+
+
+  return 0;
+
+}
