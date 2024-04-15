82cddf7fcf34cb0c21b6a8395404c47019c8be86
Vinicius Paranho Ribeiro <viniciusparanho@gmail.com>
2024-04-15 01:31:37 -0300

    Create atividade4.c

---
+++ atividade4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+
+int base;
+  int altura;
+  int area;
+
+  printf("digite a base:");
+  scanf("%i", &base);
+
+  printf("digite a altura");
+  scanf("%i",&altura);
+
+  area = base*altura;
+
+  printf("A area é :%i\n",area);
+  
+
+
+
+
+
+
+
+  return 0;
+  
+}
