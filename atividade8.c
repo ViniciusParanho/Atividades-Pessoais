0f92b186eb5416436f59431eb49b564bc204d1a7
Vinicius Paranho Ribeiro <viniciusparanho@gmail.com>
2024-04-15 01:34:12 -0300

    Create atividade8.c

---
+++ atividade8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+
+int fatorial=5;
+  int resposta=1;
+
+  for (;fatorial>=1; -- fatorial){
+
+
+ resposta = resposta * fatorial;
+
+    printf ("%i\n",resposta);
+
+
+  }
+
+
+
+
+
+
+
+  return 0;
+
+}
