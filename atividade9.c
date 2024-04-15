8d46f9d8629ff773a023e277804ce60b813ff5b6
Vinicius Paranho Ribeiro <viniciusparanho@gmail.com>
2024-04-15 01:34:43 -0300

    Create atividade9.c

---
+++ atividade9.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+int idade;
+  printf("Favor digitar sua idade:\n");
+  scanf("%i", &idade);
+
+  if (idade <18){
+printf(" Bebidas não liberadas\n");
+
+
+
+  } else {
+
+
+  printf (" Bebidas Liberadas\n");
+
+  }
+
+
+
+
+
+
+
+
+  return 0;
+
+}
